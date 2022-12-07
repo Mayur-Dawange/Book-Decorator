dff5c5944970093260bf1408b7d5b4e21bf048c7
Mayur <mdawange@techstitcher.com>
2022-12-07 13:22:25 +0530

    update map section

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -3,14 +3,20 @@ import BannerStyled from "../Banner/BannerStyled";
 import HowDoWe from "../HowDoWe/HowDoWe";
 import HowItWorks from "../HowItWorks/HowItWorks";
 import MapSection from "../MapSection/MapSection";
+import ProjectSlider from "../ProjectSlider/ProjectSlider";
+import ReadyToBook from "../ReadyToBook/ReadyToBook";
+import Testimonialslider from "../Testimonialslider/Testimonialslider";
 
 const Home = () => {
   return (
     <div>
       <BannerStyled />
+      <ProjectSlider />
       <HowItWorks />
       <HowDoWe />
+      <Testimonialslider />
       <MapSection />
+      <ReadyToBook />
     </div>
   );
 };
